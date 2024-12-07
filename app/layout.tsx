b101a8cf64ae29833c19e414a7a72f66a293842c
Tom Wu <80656252+wujinwang@users.noreply.github.com>
2024-12-06 18:16:55 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <div className="text-lg">By Jinwang Wu</div>
         <div className="text-lg">wujw2013@gmail.com</div>
         <div className="mt-4">Frontend source code: <a href="https://github.com/wujinwang/vehicle-dashboard-react" className="text-red-600">React/Next/Tailwind</a></div>
-        <div className="mt-1">Backend source code: <a href="https://github.com/wujinwang/vehicle-dashboard-java" className="text-red-600">Java/Spring boot</a></div>
+        <div className="mt-1">Backend source code: <a href="https://github.com/wujinwang/vehicle-dashboard-java" className="text-red-600">Java/Spring boot/MySQL</a></div>
       </footer>
       </body>
     </html>
